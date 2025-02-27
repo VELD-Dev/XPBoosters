c060aa80cba42a8d8457e091f70750ebec0b4f6e
V E L D <evan.robin07@gmail.com>
2025-02-27 04:04:40 +0100

    Set version number to 1.0.0

--- src/main/java/com/velddev/xpboosters/Main.java
+++ src/main/java/com/velddev/xpboosters/Main.java
@@ -9,7 +9,7 @@ import org.slf4j.LoggerFactory;
 public class Main implements ModInitializer {
 
     public static final String MOD_ID = "xpboosters";
-    public static final String MOD_VERSION = "0.1.0";
+    public static final String MOD_VERSION = "1.0.0";
 
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
